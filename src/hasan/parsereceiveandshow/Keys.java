a262e20c498f26d6e2ffb1f63262c973d3283b18
HasanFaraaz <hasanandroidapp@gmail.com>
2015-03-21 18:01:18 +0530

    Update Keys.java
    CHANGED KEYS

--- src/hasan/parsereceiveandshow/Keys.java
+++ src/hasan/parsereceiveandshow/Keys.java
@@ -2,6 +2,6 @@ package hasan.parsereceiveandshow;
 
 public class Keys {
 	// Fill in your keys
-	protected static final String applicationId = "dJbT6nSwb12EXdVQ1eagG96BMr1ebFWzF1hVsMTy";
-	protected static final String clientKey = "Sj00vUmoZUlUXp9ZvFAa26NceRvoJQsQTJTgCWOB";
+	protected static final String applicationId = "GET KEYS FROM PARSE";
+	protected static final String clientKey = "GET KEYS FROM PARSE";
 }
